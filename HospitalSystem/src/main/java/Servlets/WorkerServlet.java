68d3cef9e02da623b00630f819ee5ec124a8db55
Manos Chatzakis <chatzakis@ics.forth.gr>
2021-01-08 16:02:49 +0200

    ηξ

--- HospitalSystem/src/main/java/Servlets/WorkerServlet.java
+++ HospitalSystem/src/main/java/Servlets/WorkerServlet.java
@@ -13,15 +13,13 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.json.JSONObject;
 
 /**
  *
  * @author George
  */
-public class WorkerServlet extends HttpServlet
-{
-
-    
+public class WorkerServlet extends HttpServlet {
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
@@ -33,13 +31,49 @@ public class WorkerServlet extends HttpServlet
      * @throws IOException if an I/O error occurs
      */
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException
-    {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("WorkerSite.jsp").forward(request, response);
+    }
 
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        JSONObject obj = new JSONObject();
+
+        int currentDutyTime = 2;
+        int workerID = (Integer) request.getSession(false).getAttribute("user_id");
+        int request_id = Integer.parseInt(request.getParameter("requestID"));
+
+        System.out.println("request id is  : " + request_id);
+
+        try {
+
+            PrintWriter out = response.getWriter();
+
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+
+            switch (request_id) {
+            case 1:
+                //obj = getPersonalAndDrugInfo(doctorID, currentDutyTime);
+                out.print(obj);
+                out.flush();
+                System.out.println(obj.toString(0));
+                break;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     
-
+    
 }
