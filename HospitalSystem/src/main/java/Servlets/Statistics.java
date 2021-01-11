53446869a90a1637f132515a136cd0eb7be7c382
Manos Chatzakis <chatzakis@ics.forth.gr>
2021-01-11 22:28:36 +0200

    Update Statistics.java

--- HospitalSystem/src/main/java/Servlets/Statistics.java
+++ HospitalSystem/src/main/java/Servlets/Statistics.java
@@ -23,8 +23,7 @@ import org.json.JSONObject;
  *
  * @author George
  */
-public class Statistics extends HttpServlet
-{
+public class Statistics extends HttpServlet {
 
     int MONTHLY_DUTY_STATS = 1;
     int DAILY_DUTY_STATS = 2;
@@ -32,8 +31,7 @@ public class Statistics extends HttpServlet
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException
-    {
+            throws ServletException, IOException {
         request.getRequestDispatcher("Statistics.jsp").forward(request, response);
 
     }
@@ -48,59 +46,42 @@ public class Statistics extends HttpServlet
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException
-    {
+            throws ServletException, IOException {
         System.out.println("statistics table");
         PrintWriter out = response.getWriter();
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
         int request_id = Integer.parseInt(request.getParameter("requestID"));
-        if (request_id == MONTHLY_DUTY_STATS)
-        {
+        if (request_id == MONTHLY_DUTY_STATS) {
             System.out.println("MONTHLY_DUTY_STATS");
 
-            try
-            {
-                String  month = request.getParameter("month");
+            try {
+                String month = request.getParameter("month");
                 JSONArray dutyArrray = CreateDutyMonthlyStatistics(month, month);
                 out.print(dutyArrray);
                 out.flush();
                 System.out.println(dutyArrray.toString(0));
-            }
-            catch (SQLException ex)
-            {
+            } catch (SQLException ex) {
+                Logger.getLogger(Statistics.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (ClassNotFoundException ex) {
                 Logger.getLogger(Statistics.class.getName()).log(Level.SEVERE, null, ex);
             }
-            catch (ClassNotFoundException ex)
-            {
-                Logger.getLogger(Statistics.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-        else if (request_id == DAILY_DUTY_STATS)
-        {
+        } else if (request_id == DAILY_DUTY_STATS) {
             System.out.println("Daily_duty_stats");
 
-            try
-            {
+            try {
                 JSONArray dutyArrray = CreateDutyDailyStatistics("2021-01-06", "2021-01-06");
                 out.print(dutyArrray);
                 out.flush();
                 System.out.println(dutyArrray.toString(0));
-            }
-            catch (SQLException ex)
-            {
+            } catch (SQLException ex) {
+                Logger.getLogger(Statistics.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (ClassNotFoundException ex) {
                 Logger.getLogger(Statistics.class.getName()).log(Level.SEVERE, null, ex);
             }
-            catch (ClassNotFoundException ex)
-            {
-                Logger.getLogger(Statistics.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-        else if (request_id == COVID_19_REPORT)
-        {
+        } else if (request_id == COVID_19_REPORT) {
             JSONObject obj = new JSONObject();
-            try
-            {
+            try {
                 PrintWriter out2 = response.getWriter();
                 response.setContentType("application/json");
                 response.setCharacterEncoding("UTF-8");
@@ -108,13 +89,9 @@ public class Statistics extends HttpServlet
                 out2.print(obj);
                 out2.flush();
                 System.out.println(obj.toString());
-            }
-            catch (SQLException ex)
-            {
+            } catch (SQLException ex) {
                 Logger.getLogger(Statistics.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            catch (ClassNotFoundException ex)
-            {
+            } catch (ClassNotFoundException ex) {
                 Logger.getLogger(Statistics.class.getName()).log(Level.SEVERE, null, ex);
             }
 
@@ -122,8 +99,7 @@ public class Statistics extends HttpServlet
 
     }
 
-    public JSONObject CreateCovidReport() throws SQLException, ClassNotFoundException
-    {
+    public JSONObject CreateCovidReport() throws SQLException, ClassNotFoundException {
         DBConnection conn;
 
         conn = new DBConnection();
@@ -137,6 +113,7 @@ public class Statistics extends HttpServlet
                 + "INNER JOIN examinations ON examinations.patient_id= patients.patient_id  AND examinations.illness_id=" + 1 + " \n"
                 + "INNER JOIN visit ON visit.visit_id = examinations.visit_id\n";
 
+        //eisai malakas
         String chronicDiseasesQuery = "SELECT visit.visit_id,patients_chronic_diseases.disease \n"
                 + "FROM patients\n"
                 + "INNER JOIN examinations ON examinations.patient_id= patients.patient_id  AND examinations.illness_id=" + 1 + " \n"
@@ -145,8 +122,7 @@ public class Statistics extends HttpServlet
 
         res = conn.executeQuery(covidPersonalInformationQuery);
         int counter = 0;
-        while (res != null && res.next())
-        {
+        while (res != null && res.next()) {
             int vis_id = res.getInt("visit_id");
             obj.put("visit" + counter, vis_id);
             obj.put("name" + counter, res.getString("name"));
@@ -157,17 +133,14 @@ public class Statistics extends HttpServlet
 
             res2 = conn.executeQuery(chronicDiseasesQuery);
             String chronicDieseases = "";
-            while (res2 != null && res2.next())
-            {
-                if (vis_id == res2.getInt("visit_id"))
-                {
+            while (res2 != null && res2.next()) {
+                if (vis_id == res2.getInt("visit_id")) {
                     chronicDieseases += res2.getString("disease");
                     chronicDieseases += ",";
                 }
 
             }
-            if (!chronicDieseases.equals(""))
-            {
+            if (!chronicDieseases.equals("")) {
                 chronicDieseases = chronicDieseases.substring(0, chronicDieseases.length() - 1);
                 obj.put("diseases" + counter, chronicDieseases);
 
@@ -181,8 +154,7 @@ public class Statistics extends HttpServlet
         return obj;
     }
 
-    public JSONArray CreateDutyDailyStatistics(String from, String to) throws SQLException, ClassNotFoundException
-    {
+    public JSONArray CreateDutyDailyStatistics(String from, String to) throws SQLException, ClassNotFoundException {
         DBConnection conn = new DBConnection();
         JSONArray dutyArrray = new JSONArray();
         ResultSet res = null;
@@ -194,8 +166,7 @@ public class Statistics extends HttpServlet
                 + "\'" + from + "\'" + ")AND (" + "\'" + to + "\'" + ");";
 
         res = conn.executeQuery(drugsQuery);
-        while (res != null && res.next())
-        {
+        while (res != null && res.next()) {
             dutyArrray.put(res.getInt("drugs_amount"));
         }
         String incidentsQuery = "SELECT COUNT(examinations.visit_id) AS incidents_amount FROM examinations\n"
@@ -203,8 +174,7 @@ public class Statistics extends HttpServlet
                 + "\'" + from + "\'" + ")AND (" + "\'" + to + "\'" + ");";
 
         res = conn.executeQuery(incidentsQuery);
-        while (res != null && res.next())
-        {
+        while (res != null && res.next()) {
             dutyArrray.put(res.getInt("incidents_amount"));
         }
         String examinationsQuery = "SELECT COUNT(examinations.exam_id)  AS examinations_amount FROM examinations\n"
@@ -212,8 +182,7 @@ public class Statistics extends HttpServlet
                 + "\'" + from + "\'" + ")AND (" + "\'" + to + "\'" + ");";
 
         res = conn.executeQuery(examinationsQuery);
-        while (res != null && res.next())
-        {
+        while (res != null && res.next()) {
             dutyArrray.put(res.getInt("examinations_amount"));
         }
         String illnessQuery = "SELECT COUNT(examinations.illness_id)  AS illnesses_amount FROM examinations\n"
@@ -221,8 +190,7 @@ public class Statistics extends HttpServlet
                 + "\'" + from + "\'" + ")AND (" + "\'" + to + "\'" + ");";
 
         res = conn.executeQuery(illnessQuery);
-        while (res != null && res.next())
-        {
+        while (res != null && res.next()) {
             dutyArrray.put(res.getInt("illnesses_amount"));
         }
 
@@ -231,8 +199,7 @@ public class Statistics extends HttpServlet
         return dutyArrray;
     }
 
-    public JSONArray CreateDutyMonthlyStatistics(String from, String to) throws SQLException, ClassNotFoundException
-    {
+    public JSONArray CreateDutyMonthlyStatistics(String from, String to) throws SQLException, ClassNotFoundException {
         DBConnection conn = new DBConnection();
         JSONArray dutyArrray = new JSONArray();
         ResultSet res = null;
@@ -244,8 +211,7 @@ public class Statistics extends HttpServlet
                 + "\'" + from + "\'" + ")AND MONTH(" + "\'" + to + "\'" + ");";
 
         res = conn.executeQuery(drugsQuery);
-        while (res != null && res.next())
-        {
+        while (res != null && res.next()) {
             dutyArrray.put(res.getInt("drugs_amount"));
         }
         String incidentsQuery = "SELECT COUNT(examinations.visit_id) AS incidents_amount FROM examinations\n"
@@ -253,8 +219,7 @@ public class Statistics extends HttpServlet
                 + "\'" + from + "\'" + ")AND MONTH(" + "\'" + to + "\'" + ");";
 
         res = conn.executeQuery(incidentsQuery);
-        while (res != null && res.next())
-        {
+        while (res != null && res.next()) {
             dutyArrray.put(res.getInt("incidents_amount"));
         }
         String examinationsQuery = "SELECT COUNT(examinations.exam_id)  AS examinations_amount FROM examinations\n"
@@ -262,8 +227,7 @@ public class Statistics extends HttpServlet
                 + "\'" + from + "\'" + ")AND MONTH(" + "\'" + to + "\'" + ");";
 
         res = conn.executeQuery(examinationsQuery);
-        while (res != null && res.next())
-        {
+        while (res != null && res.next()) {
             dutyArrray.put(res.getInt("examinations_amount"));
         }
         String illnessQuery = "SELECT COUNT(examinations.illness_id)  AS illnesses_amount FROM examinations\n"
@@ -271,14 +235,83 @@ public class Statistics extends HttpServlet
                 + "\'" + from + "\'" + ")AND MONTH(" + "\'" + to + "\'" + ");";
 
         res = conn.executeQuery(illnessQuery);
-        while (res != null && res.next())
-        {
+        while (res != null && res.next()) {
             dutyArrray.put(res.getInt("illnesses_amount"));
         }
 
         conn.closeDBConnection();
 
         return dutyArrray;
+    }
+
+    public int getCountOfDrugs(String date, int drugID) throws SQLException, ClassNotFoundException {
+        /*String query = "SELECT COUNT(examinations.drug_id)\n"
+                + "FROM examinations\n"
+                + "WHERE DATE(examinations.date) = \"" + date + "\" AND examinations.drug_id = " + drugID + ";";*/
+        String query = "SELECT COUNT(examinations.drug_id)\n"
+                + "FROM examinations\n"
+                + "INNER JOIN visit ON examinations.visit_id = visit.visit_id\n"
+                + "INNER JOIN dutytime ON dutytime.dutytime_id = visit.dutytime_id\n"
+                + "WHERE DATE(dutytime.date) = \"" + date + "\" AND examinations.drug_id = " + drugID + "";
+        DBConnection conn = new DBConnection();
+        ResultSet res = null;
+        int count = 0;
+
+        res = conn.executeQuery(query);
+        while (res != null && res.next()) {
+            count = res.getInt(1);
+        }
+
+        return count;
+
+    }
+
+    public int getCountOfIllnesses(String date, int illnessID) throws SQLException, ClassNotFoundException {
+        /*String query = "SELECT COUNT(examinations.illness_id)\n"
+                + "FROM examinations\n"
+                + "WHERE DATE(examinations.date) = \"" + date + "\" AND examinations.illness_id = " + illnessID + ";";*/
+        String query = "SELECT COUNT(examinations.illness_id)\n"
+                + "FROM examinations\n"
+                + "INNER JOIN visit ON examinations.visit_id = visit.visit_id\n"
+                + "INNER JOIN dutytime ON dutytime.dutytime_id = visit.dutytime_id\n"
+                + "WHERE DATE(dutytime.date) = \"" + date + "\" AND examinations.illness_id = " + illnessID + ";";
+
+        DBConnection conn = new DBConnection();
+        ResultSet res = null;
+        int count = 0;
+
+        res = conn.executeQuery(query);
+        while (res != null && res.next()) {
+            count = res.getInt(1);
+        }
+
+        return count;
+
+    }
+
+    public int getCountOfVisits(String date) throws SQLException, ClassNotFoundException {
+        String query = "SELECT COUNT(visit.visit_id)\n"
+                + "FROM visit\n"
+                + "INNER JOIN dutytime ON visit.dutytime_id = dutytime.dutytime_id\n"
+                + "WHERE DATE(dutytime.date) = \"" + date + "\";";
+
+        DBConnection conn = new DBConnection();
+        ResultSet res = null;
+        int count = 0;
+
+        res = conn.executeQuery(query);
+        while (res != null && res.next()) {
+            count = res.getInt(1);
+        }
+
+        return count;
+    }
+    
+    public String getChronicDisOfPatient(int patientID) {
+        String query = "SELECT patients_chronic_diseases.disease\n"
+                + "FROM patients_chronic_diseases\n"
+                + "WHERE patients_chronic_diseases.patient_id = " + patientID + ";";
+        return query;
     }
 
 }
